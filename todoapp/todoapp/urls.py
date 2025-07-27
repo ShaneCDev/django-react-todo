51ccae95b48b63662ff40f76a10cf2daa5393253
Shane <36411961+ShaneCDev@users.noreply.github.com>
2025-07-27 17:42:06 +0100

    Added API routing in urls.py

--- todoapp/todoapp/urls.py
+++ todoapp/todoapp/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from items import views
+
+
+router = routers.DefaultRouter()
+router.register(r'items', views.TodoItemView, 'todoitem')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
